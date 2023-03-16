ff329d56ca7d46032a12c53660656784548b2b6c
Antonio Pirani <s1098080@studenti.univpm.it>
2023-03-16 13:41:23 +0100

    Initial Commit

---
+++ group7/group7App.swift
@@ -0,0 +1,17 @@
+//
+//  group7App.swift
+//  group7
+//
+//  Created by Antonio Pirani on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct group7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
